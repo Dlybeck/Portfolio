7e3651bd48dc2a103a464be7988c4fdbbcaf3e76
dlybeck <dlybeck383@gmail.com>
2025-01-10 20:11:30 -0800

    Back button functionality added. Also nba project is added

--- static/scripts/mini-pages/openPage.js
+++ static/scripts/mini-pages/openPage.js
@@ -3,19 +3,36 @@ function openPage(route) {
     const page = document.querySelector(".mini-window");
     const closeButton = document.querySelector(".close-button");
     
+    // Store the initial route
+    const initialRoute = route;
+
+    // Set the iframe's src to the initial route
     page.setAttribute("src", route);
     container.style.visibility = "visible";
     container.style.opacity = "1";
 
     const closePage = () => {
-        container.style.opacity = "0";
-        container.style.visibility = "hidden";
-        document.removeEventListener('click', handleClickOutside);
+        // Only close page if the page is visible
+        if (container.style.visibility === "visible" && container.style.opacity === "1") {
+            // make the close button act as a back button IF it is not on the original page
+            if (page.contentWindow.location.pathname === initialRoute) {
+                // hide
+                container.style.opacity = "0";
+                container.style.visibility = "hidden";
+                document.removeEventListener('click', handleClickOutside);
+            } else {
+                // go back to the previous page
+                page.contentWindow.history.back();
+            }
+        }
     };
+    
 
     const handleClickOutside = (event) => {
         if (!container.contains(event.target)) {
-            closePage();
+            container.style.opacity = "0";
+            container.style.visibility = "hidden";
+            document.removeEventListener('click', handleClickOutside);
         }
     };
 
